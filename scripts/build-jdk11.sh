8fa29a5fccf61cf3b8038a7c119c99807069111b
Martin Goldhahn <mgoldhahn@gmail.com>
2020-08-15 15:19:26 +0200

    sonar and site only for non pull requests

--- scripts/build-jdk11.sh
+++ scripts/build-jdk11.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  MAVEN_OPTS="--add-modules ALL-SYSTEM" mvn clean install sonar:sonar site site-deploy
+  MAVEN_OPTS="--add-modules ALL-SYSTEM" mvn clean install sonar:sonar
 else
   MAVEN_OPTS="--add-modules ALL-SYSTEM" mvn clean verify
 fi
