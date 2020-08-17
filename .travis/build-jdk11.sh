3dca62ef3ce894c427c85d6f2cf0e86288bb0bd0
Martin Goldhahn <mgoldhahn@gmail.com>
2020-08-17 23:04:35 +0200

    typo in build script

--- .travis/build-jdk11.sh
+++ .travis/build-jdk11.sh
@@ -2,7 +2,7 @@
 set -ev
 export MAVEN_OPTS="--add-modules ALL-SYSTEM"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  mkdir -f $HOME/.m2
+  mkdir -p $HOME/.m2
   cp .travis/settings.xml $HOME/.m2/settings.xml
   mvn -B clean install sonar:sonar site site-deploy
 else
